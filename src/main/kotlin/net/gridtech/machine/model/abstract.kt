bba7328eec6e79c046a401c7283986606a39e22d
Dexter Yang <dexter.yang@qq.com>
2019-07-03 18:54:38 +0800

    init

--- src/main/kotlin/net/gridtech/machine/model/abstract.kt
+++ src/main/kotlin/net/gridtech/machine/model/abstract.kt
@@ -3,31 +3,31 @@ package net.gridtech.machine.model
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 import net.gridtech.core.data.ChangedType
-import net.gridtech.core.data.INodeClass
+import net.gridtech.core.data.IStructureData
 
-abstract class EntityClass(initNodeClass: INodeClass, observable: Observable<Triple<ChangedType, String, INodeClass?>>) {
-    val id = initNodeClass.id
-    private val updatePublisher = Observable.concat(
-            Observable.just(initNodeClass),
+abstract class IBaseStructure<T : IStructureData>(initData: T, observable: Observable<Triple<ChangedType, String, T?>>) {
+
+    val id = initData.id
+
+    protected val updatePublisher = Observable.concat(
+            Observable.just(initData),
             observable.filter { it.first == ChangedType.UPDATE && it.second == id }.map { it.third!! }
     ).publish()
 
-
-    private val deleteObservable = observable.filter { it.first == ChangedType.DELETE && it.second == id }.firstOrError()
-    private lateinit var updateDisposable: Disposable
-
-    init {
-        observable.doOnEach { }
-    }
+    private val deletePublisher = observable.filter { it.first == ChangedType.DELETE && it.second == id }.publish()
+    private var updateDisposable: Disposable? = null
+    private var deleteDisposable: Disposable? = null
 
     fun start() {
-        updateDisposable = updatePublisher.connect()
-        deleteObservable.subscribe { _, _ ->
-            updateDisposable.dispose()
+        deletePublisher.subscribe {
+            updateDisposable?.dispose()
+            deleteDisposable?.dispose()
         }
+        updateDisposable = updatePublisher.connect()
+        deleteDisposable = deletePublisher.connect()
     }
 
     fun onDelete(cleanUpFunction: (id: String) -> Unit) {
-        deleteObservable.subscribe { _, _ -> cleanUpFunction(id) }
+        deletePublisher.doOnNext { cleanUpFunction(id) }
     }
 }
